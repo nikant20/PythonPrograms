24fde5049cb99ed3c032bc151b968335715581f0
nikant20@gmail.com <nikant20@gmail.com>
2016-12-21 07:41:22 +0530

    Adding arithmetic operators example

---
+++ ArithmeticOperators.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a**b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
